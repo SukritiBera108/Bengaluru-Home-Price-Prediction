989431029a3f40369fe805312e91ee7e95ceb998
SukritiBera108 <sukritibera108@gmail.com>
2025-11-13 12:57:17 +0530

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify, render_template
+import util
+
+app = Flask(__name__, static_url_path="/static")
+
+@app.route('/')
+def home():
+    return render_template('app.html', prediction_text='', is_error=False)
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({'locations': util.get_location_names()})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    try:
+        total_sqft = float(request.form['total_sqft'])
+        location = request.form['location']
+        bhk = int(request.form['bhk'])
+        bath = int(request.form['bath'])
+        estimated_price = util.get_estimated_price(location, total_sqft, bath, bhk)
+
+        if estimated_price > 0:
+            if estimated_price >= 100:
+                crore_value = estimated_price / 100
+                prediction_text = f"Estimated Price: {crore_value:,.2f} Crore (INR)"
+            else:
+                prediction_text = f"Estimated Price: {estimated_price:,.2f} Lakh (INR)"
+            is_error = False
+        else:
+            prediction_text = 'Invalid Input. Unrealistic House for that place.'
+            is_error = True
+
+    except (ValueError, KeyError):
+        prediction_text = 'Invalid User Input.'
+        is_error = True
+
+    return render_template(
+        'app.html',
+        prediction_text=prediction_text,
+        is_error=is_error,
+        request=request
+    )
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run(debug=True)
+
---
+++ server/util.py
@@ -0,0 +1,72 @@
+import pickle
+import json
+import numpy as np
+
+
+__locations = None
+__data_columns = None
+__model = None
+
+def get_estimated_price(location, sqft, bath, bhk):
+    """
+    Predicts the price based on user input.
+    """
+    try:
+        
+        loc_index = __data_columns.index(location.lower())
+    except ValueError:
+        
+        loc_index = -1 
+
+    
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+   
+    return round(__model.predict([x])[0], 2)
+
+def load_saved_artifacts():
+    """
+    Loads the saved model and column information from disk.
+    """
+    print("loading saved artifacts...start")
+    global __data_columns
+    global __locations
+    global __model
+
+  
+    with open("./server/artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)['data_columns']
+  
+        __locations = __data_columns[3:]
+
+    
+    if __model is None:
+        with open('./server/artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+            __model = pickle.load(f)
+    
+    print("loading saved artifacts...done")
+
+def get_location_names():
+    """
+    Returns the list of location names.
+    """
+    return __locations
+
+def get_data_columns():
+    """
+    Returns the list of all data columns.
+    """
+    return __data_columns
+
+if __name__ == '__main__':
+    load_saved_artifacts()
+    print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) 
+    print(get_estimated_price('Ejipura', 1000, 2, 2)) 
